63025487253d3b6d7b05ab9b7977375405914406
Kemal <Kemal>
2022-05-07 08:03:32 +0200

    Test commit

--- TaskManager/Shared/AuditableEntitiesManager.cs
+++ TaskManager/Shared/AuditableEntitiesManager.cs
@@ -2,6 +2,7 @@
 
 namespace TaskManager.Shared
 {
+    // this is a test comment
     public interface AuditableEntitiesManager
     {
         public void ProcessBeforeSave(ChangeTracker changeTracker, User currentUser);
